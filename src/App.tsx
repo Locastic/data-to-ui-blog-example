bcc40b0decd41b4001b4390b58ec50d28ba80109
Luka Smolcic <lukasmolcic@MacBook-Pro-od-Luka.local>
2020-02-21 17:40:26 +0100

    tags

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,25 @@ import BlogPost from './components/blogPost';
 function App() {
   return (
     <BlogPost
-      title="Post title"
+      title="Featured post title"
       excerpt="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, dicta iure at incidunt debitis voluptates sit alias voluptatem inventore, numquam sint nisi aperiam aut voluptate similique, ullam quas placeat magni!"
       image={{
         path: "//picsum.photos/700/600",
         title: "Image title",
         alt: "Image alt text"
       }}
-      published_at={new Date(2020, 22,2)}
+      published_at={new Date(2020, 22, 2)}
+      featured={true}
+      tags={[
+        {
+          title: "Tag 1",
+          path: "/tag1"
+        },
+        {
+          title: "Tag 2",
+          path: "/tag2"
+        }
+      ]}
     />
   );
 }
--- src/components/blogPost.tsx
+++ src/components/blogPost.tsx
@@ -10,21 +10,22 @@ interface Props {
     }
     featured?: boolean,
     published_at: Date
-    tags?: [
+    tags?: Array<
         {
             title: string,
             path: string
         }
-    ]
+    >
 }
 
 export default function BlogPost({ title, excerpt, image, featured, published_at, tags }: Props) {
     return (
-        <article>
+        <article className={(featured) ? 'featured' : ''}>
             <img src={image.path} alt={image.alt} title={title} />
             <h3>{title}</h3>
             <time>{published_at.toDateString()}</time>
             <p>{excerpt}</p>
+            {tags && tags.map(tag => <a href={tag.path}>{tag.title}</a>)}
         </article>
     )
 }
